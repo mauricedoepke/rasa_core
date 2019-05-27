e07ecacaa0bd4d7f69dd08948089cafb90262767
Maurice Doepke <mauricedoepke@outlook.de>
2019-05-27 11:26:35 +0200

    Domain now collects intent properties itself

--- tests/core/test_actions.py
+++ tests/core/test_actions.py
@@ -44,7 +44,7 @@ def test_action_instantiation_from_names():
 
 def test_domain_action_instantiation():
     domain = Domain(
-        intent_properties={},
+        intent_properties=[],
         entities=[],
         slots=[],
         templates={},
